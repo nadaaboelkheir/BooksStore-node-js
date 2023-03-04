b2f8d8754be694413793822e6eb7088660213533
nadaaboelkheir <nadaaboelkheir5@gmail.com>
2023-03-04 02:40:15 +0200

    first commit

---
+++ model/bookModel.js
@@ -0,0 +1,10 @@
+exports.Note = class Note {
+    constructor(book_id, book_title, book_description, publisher, author, isbn) {
+        this.id = id
+        this.title = title
+        this.content = content
+        this.createdby = createdby
+        this.date = date
+    }
+
+}
